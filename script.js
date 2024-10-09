008fbc1297ea4833e32137091d7fcb65fe67d678
nelsinthebuilding <nzongonelson@gmail.com>
2024-10-09 11:28:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+    // Smooth Scrolling Effect for Anchor Links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+</script>
